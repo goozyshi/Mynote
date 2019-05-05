57e1d0f52dc814e0a437bead1bf7f969a8682783
goozyshi <goozyshi@gmail.com>
2019-05-05 10:57:59 +0800

    20190505

--- D&A/test.js
+++ D&A/test.js
@@ -1,13 +1,22 @@
 /**
+ * @param {number} s
  * @param {number[]} nums
  * @return {number}
  */
-var arrayPairSum = function(nums) {
-  let result = 0
-  nums.sort((a,b)=>a-b)
-  for (let i = 0; i < nums.length;) {
-    result += nums[i]
-    i+=2;
+var minSubArrayLen = function(s, nums) {
+  if (s === null || nums.length === 0) {
+    return 0
   }
-  return result
-}
+  let l = 0
+  let r = 0
+  let sum = 0
+  let min = Infinity
+  while (r < nums.length) {
+    sum += nums[r ++]
+    while (sum >= s) {
+      min = Math.min(min, r-l)
+      sum -= nums[l++]
+    }
+  }
+  return min==Infinity?0:min
+};
